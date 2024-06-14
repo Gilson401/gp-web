2635b3d64b203fdd00c573ff588aadb285a82553
Gilson Santos <gilson.santos@infobase.com.br>
2024-06-14 15:59:52 -0300

    modal mobile menu

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
+const menuButton = document.getElementById('hamburgmenu');
+const favDialog = document.getElementById('favDialog');
 
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
+
+menuButton.addEventListener('click', () => {
+  favDialog.showModal();
 });
 
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
-});
+favDialog.addEventListener('click', () => {
+    favDialog.close();
+  });
